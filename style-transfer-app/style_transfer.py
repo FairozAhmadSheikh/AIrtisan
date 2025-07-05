4fa737d73b726d6fca83f83f28c8eb6342e6b463
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-07-05 18:08:06 +0530

    c2

--- style-transfer-app/style_transfer.py
+++ style-transfer-app/style_transfer.py
@@ -5,3 +5,13 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 import copy
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Preprocess images
+loader = transforms.Compose([
+    transforms.Resize((512, 512)),
+    transforms.ToTensor(),
+    lambda x: x[:3, :, :],  # remove alpha if present
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
